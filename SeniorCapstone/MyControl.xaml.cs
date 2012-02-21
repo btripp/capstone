fae8d6ff881cc62a25d997cf4b2b5d24d867b900
Chris Thompson <cthomp@live.com>
2012-02-20 21:41:41 -0500

    Hey, this is just getting us in the direction. im not even sure if this is the right thing that we need to be doing but at least i finally worked on something. ill facebook more detail cuz im not even sure where you would read this

--- SeniorCapstone/MyControl.xaml.cs
+++ SeniorCapstone/MyControl.xaml.cs
@@ -12,12 +12,17 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using Microsoft.TeamFoundation.Framework.Client;
+using Microsoft.TeamFoundation.Client;
+
+
 
 namespace AugustaStateUniversity.SeniorCapstone
 {
     /// <summary>
     /// Interaction logic for MyControl.xaml
     /// </summary>
+    /// 
     public partial class MyControl : UserControl
     {
 
@@ -31,9 +36,42 @@ namespace AugustaStateUniversity.SeniorCapstone
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions")]
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(string.Format(System.Globalization.CultureInfo.CurrentUICulture, "We are inside {0}.button1_Click()", this.ToString()),
-                            "Latest changed");
+            TreeView myTree = treeView1;
+            //Visual myVisual = treeView1;
+            //Visual childVisual = (Visual)VisualTreeHelper.GetChild(myVisual, 0);
+            //void WalkDownLogicalTree(object current)
+            //{
+            //    DoSomethingWithObjectInLogicalTree(current);
 
+            //    // The logical tree can contain any type of object, not just 
+            //    // instances of DependencyObject subclasses.  LogicalTreeHelper
+            //    // only works with DependencyObject subclasses, so we must be
+            //    // sure that we do not pass it an object of the wrong type.
+            //    DependencyObject depObj = current as DependencyObject;
+
+            //    if (depObj != null)
+            //        foreach(object logicalChild in LogicalTreeHelper.GetChildren(depObj))
+            //            WalkDownLogicalTree(logicalChild);
+            //}
+            TreeViewItem node = (TreeViewItem)LogicalTreeHelper.FindLogicalNode(myTree, "three");
+            int i = 0;
+            string msg = "";
+            foreach (TreeViewItem nodes in node.Items)
+            {
+                i++;
+                if (nodes.HasItems)
+                {
+                    msg += " AND it has kids!";
+                }
+                MessageBox.Show("child "+i+" is "+nodes.Header.ToString()+"."+ msg);
+            }
+            
+
+        }
+
+        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            
         }
        
 
